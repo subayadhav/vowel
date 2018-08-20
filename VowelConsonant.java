7cece0f20a89fb5f276e3f1a344389461d885a1b
subayadhav <suba10499@gmail.com>
2018-08-20 14:05:54 +0530

    Create VowelConsonant.java

---
+++ VowelConsonant.java
@@ -0,0 +1,15 @@
+class VowelConsants
+{
+  public static void main(String args[])
+  {
+  char='f';
+  if(f=='a'||f=='e'||f=='i'||f=='o'||f=='u')
+  {
+    System.out.println("the given letter is vowel");
+    }
+    else
+    {
+      System.out.println("the given letter is consonants");
+     }
+     }
+     }
